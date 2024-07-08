5b5f519b14eab2768bb0d074d134c97c8c8eaa2b
DariusChao <53123695+DariusChao@users.noreply.github.com>
2024-07-07 22:06:40 -0700

    Update __init__.py

--- spanet/__init__.py
+++ spanet/__init__.py
@@ -1,4 +1,3 @@
 from spanet.network.jet_reconstruction import JetReconstructionModel
 from spanet.dataset import JetReconstructionDataset
 from spanet.options import Options
-from spanet.data_module import DataModule
